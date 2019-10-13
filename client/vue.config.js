69dc31220fa284011da92bc7c5b17d9030f53460
Samielleuch <Sami.Elleuch@insat.u-carthage.tn>
2019-10-13 15:41:14 +0100

    trying

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 module.exports = {
     //change the build dir
-    outputDir: path.resolve(__dirname , '../server/public'),
+    outputDir: path.resolve(__dirname , '../server/dist'),
     //set proxy
     devServer: {
         proxy: {
--- server/src/server.js
+++ server/src/server.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const config = require("./config/config");
 const mongoose = require("mongoose");
+const serverless = require('serverless-http');
 const path = require("path");
 const app = express();
  //MidlleWares
@@ -18,15 +19,17 @@ mongoose.connect(
   `mongodb+srv://${config.db.dbUser}:${config.db.dbPassword}@${config.db.dbName}-ubg8j.mongodb.net/test?retryWrites=true&w=majority`,
   { useNewUrlParser: true, useUnifiedTopology: true }
 );
-app.get('' , (req,res)=>res.send('hi'));
+app.get('/' , (req,res)=>res.send('hi'));
 //Handle production
 if (process.env.NODE_ENV ==='production') {
   //static
-  let a =path.resolve(__dirname ,'../server/public' ).then((name)=>  app.use(express.static(name))
+  let a =path.resolve(__dirname ,'../server/dist' ).then((name)=>  app.use(express.static(name))
 ) .catch(err => console.log(err));
   //Handle frontend
-  //app.get(/.*/, (req,res) => res.sendFile(path.resolve(__dirname ,'../public' )+'/index.html'));
+  //app.get(/.*/, (req,res) => res.sendFile(path.resolve(__dirname ,'../dist' )+'/index.html'));
 }
 app.listen(config.port, () => {
   console.log(`Server running at ${config.hostname}/`);
 });
+
+module.exports.handler = serverless(app);
